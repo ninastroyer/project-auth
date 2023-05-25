5acbbbc3a43b2a40375cbc932c774d24fb3a2a67
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-05-25 10:42:33 +0200

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from 'crypto';
+import bcrypt from 'bcrypt';
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -16,9 +18,76 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+const { Schema } = mongoose;
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String, 
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+});
+
+const User = mongoose.model("User", "UserSchema");
+
+app.post("/register", async (req,res) => {
+  const { username, password } = req.body;
+ try {
+  const salt = bcrypt.genSaltSync();
+  const newUser = await new User({
+    username: username,
+    password: bcrypt.hashSync(password, salt)
+  }).save();
+  res.status(201).json({
+    success: true,
+    response: {
+      username: newUser.username,
+      id: newUser._id,
+      accessToken: newUser.accessToken
+    }
+  })
+} catch (e) {
+  res.status(400).json({
+    success: false,
+    response: e
+  })
+}
+}) 
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({username: username})
+    // const user = await User.findOne({username})
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        response: {
+          username: user.username,
+          id: user._id,
+          accessToken: user.accessToken
+        }
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        response: "Credentials do not match"
+      });
+    }
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      response: e
+    });
+  }
 });
 
 // Start the server
